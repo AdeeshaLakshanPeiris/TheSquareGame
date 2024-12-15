9784078898ef4e6024eae53da9416434ccad80e7
Adeesha 035 <adeesha035@NIBMADMIN14s-iMac.local>
2024-12-15 12:53:07 +0530

    init

---
+++ TheSquareGame/TheSquareGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheSquareGame
+//
+//  Created by Adeesha 035 on 2024-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSquareGame/TheSquareGame/TheSquareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSquareGameApp.swift
+//  TheSquareGame
+//
+//  Created by Adeesha 035 on 2024-12-15.
+//
+
+import SwiftUI
+
+@main
+struct TheSquareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
